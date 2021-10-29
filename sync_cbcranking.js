cf2067d649abcb40e51d3fd77c157572150ae513
longkiemphi <dang.t.tin@gmail.com>
2021-10-29 13:27:00 +0700

    update file name

--- sync_cbcranking.js
+++ sync_cbcranking.js
@@ -94,3 +94,13 @@ function convertJSONToExcel(data) {
     if (typeof console !== 'undefined') console.log(new Date());
 
 };
+
+
+
+// function dynamicallyLoadScript(url) {
+//     var script = document.createElement("script"); // create a script DOM node
+//     script.src = url; // set its src to the provided URL
+
+//     document.head.appendChild(script); // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+// }
+// dynamicallyLoadScript("https://cdn.jsdelivr.net/gh/longkiemphi/Public-JS-file@main/sync_hrranking%20copy.js ")
