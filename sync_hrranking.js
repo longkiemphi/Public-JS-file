19e84dde814dbc8f5a1b80cada68c89d8ba75e16
longkiemphi <dang.t.tin@gmail.com>
2021-10-29 13:40:45 +0700

    comment code

--- sync_hrranking.js
+++ sync_hrranking.js
@@ -95,10 +95,10 @@ function convertJSONToExcel(data) {
 
 };
 
-function dynamicallyLoadScript(url) {
-    var script = document.createElement("script"); // create a script DOM node
-    script.src = url; // set its src to the provided URL
-    ​
-    document.head.appendChild(script); // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
-}
-dynamicallyLoadScript("https://cdn.jsdelivr.net/gh/longkiemphi/Public-JS-file@main/sync_hrranking.js")
+// function dynamicallyLoadScript(url) {
+//     var script = document.createElement("script"); // create a script DOM node
+//     script.src = url; // set its src to the provided URL
+//     ​
+//     document.head.appendChild(script); // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+// }
+// dynamicallyLoadScript("https://cdn.jsdelivr.net/gh/longkiemphi/Public-JS-file@main/sync_hrranking.js")
