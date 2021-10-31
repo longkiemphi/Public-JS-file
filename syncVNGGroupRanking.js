b9d3cceaa2bfa3965706756e8bf42c8f3f6fec65
longkiemphi <dang.t.tin@gmail.com>
2021-10-31 14:10:53 +0700

    syncVNGGroupRanking

--- syncVNGGroupRanking.js
+++ syncVNGGroupRanking.js
@@ -19,7 +19,7 @@ dynamicallyLoadScript("//unpkg.com/xlsx/dist/xlsx.full.min.js")
 jQuery.noConflict();
 
 
-function getRanking(teamInfo, teamName) {
+function getRanking(teamInfo, teamName, groupID) {
 
     $.ajax({
         url: "https://api.uprace.vn/api/event/rank/list",
@@ -30,8 +30,22 @@ function getRanking(teamInfo, teamName) {
             'authorization': 'Bearer ' + JSON.parse(localStorage.curentUser).accesstoken
         },
         success: function(response) {
-            console.error(response.data.list);
-            convertJSONToExcel(response.data.list, teamName)
+            var group = response.data.list;
+            switch (groupID) {
+                case 1:
+                    group = response.data.list.filter(team => team.mem <= 50)
+                    break;
+                case 2:
+                    group = response.data.list.filter(team => team.mem > 50 && team.mem <= 100)
+                    break;
+                case 3:
+                    group = response.data.list.filter(team => team.mem > 100)
+                    break;
+                default:
+                    break;
+            }
+            console.error(group);
+            convertJSONToExcel(group, teamName)
         },
         error: function(err) {
             console.error(err);
@@ -47,7 +61,7 @@ function convertJSONToExcel(data, teamName) {
     var createXLSLFormatObj = [];
 
     /* XLS Head Columns */
-    var xlsHeader = ["act", "ava", "ddis", "diff", "dis", "id", "mem", "name", "ownm", "rank", ];
+    var xlsHeader = ["act", "ownm", "ddis", "mem", "name", "rank", "diff", "id", "ava", "dis", ];
 
     /* XLS Rows Data */
     var xlsRows = data
@@ -84,21 +98,20 @@ function convertJSONToExcel(data, teamName) {
 
 };
 
-
 // function dynamicallyLoadScript(url) {
 //     var script = document.createElement("script"); // create a script DOM node
 //     script.src = url; // set its src to the provided URL
 
 //     document.head.appendChild(script); // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 // }
-// dynamicallyLoadScript("https://cdn.jsdelivr.net/gh/longkiemphi/Public-JS-file@main/syncVNGRanking.js")
+// // dynamicallyLoadScript("https://cdn.jsdelivr.net/gh/longkiemphi/Public-JS-file@main/syncVNG_Group_Ranking.js")
 
 // var teamInfo = { "trid": "4d370b18-7fcc-4e53-a334-5365367815e7", "trtm": 1635653045, "data": { "size": 100, "uid": 191607, "evid": "5", "type": 4, "value": "9", "from": 0 } }
-// var teamName = "VNG"
+// var teamName = "VNG_Group_A"
 
 // function onLoading() {
 //     if (window.XLSX) {
-//         getRanking(teamInfo, teamName);
+//         getRanking(teamInfo, teamName, 1);
 //     } else {
 //         setTimeout(function() { onLoading() }, 50);
 //     }
